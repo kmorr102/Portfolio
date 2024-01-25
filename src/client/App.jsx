a788983d0c557e4426487c0035eb466c2a8a22bd
kmorr102 <kendranmorris@gmail.com>
2024-01-25 13:05:56 -0600

    file changes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,3 +1,20 @@
-export default function App (){
+import React from 'react';
+import { Routes,Route,Link } from "react-router-dom";
+import Home from './components/home';
+import About from './components/about';
 
+function App(){
+return(
+    <div id="container">
+        <div id="navbar">
+        <Link to="/About">About me</Link>
+        </div>
+    <div>Welcome!</div>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/About" element={<About/>} />
+        </Routes>
+    </div>
+)
 }
+export default App;
