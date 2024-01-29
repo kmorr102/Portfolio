ddb958e5562e7fa5abebb4aaffd96623b9ba70de
kmorr102 <kendranmorris@gmail.com>
2024-01-29 12:18:58 -0600

    images,links and media

--- src/client/components/work.jsx
+++ src/client/components/work.jsx
@@ -13,7 +13,7 @@ function Work(){
         window.location.href= 'https://github.com/kmorr102/strangers-things'
     }
     const navigateToGirlDinnerVideo=() =>{
-        src={GirlDinnerDemo}
+        window.location.href='https://youtu.be/Z1e_ZUrt28E'
     }
     const navigateToStrangersThingsDemo=() =>{
         window.location.href='https://sage-stroopwafel-c13960.netlify.app/'
@@ -22,8 +22,8 @@ return(
     <div className='projects'>
         {/* <div className='header'> Kendra Morris </div> */}
         <img className='girlDinner' src={girlDinner} alt='girl dinner tech project' style={{width:'365px', height: '260px', border: 'black solid 3px'}}/>
-        <Link to="/Video" >
-        <button>Demo</button> </Link> <button onClick={navigateToGirlDinner}>Github</button>
+        
+        <button onClick={navigateToGirlDinnerVideo}>Demo</button>  <button onClick={navigateToGirlDinner}>Github</button>
        
         <img className='strangersThings' src={strangersThings} alt='strangers things tech project' style={{width:'365px', height: '260px', border: 'black solid 3px'}}/>
        
