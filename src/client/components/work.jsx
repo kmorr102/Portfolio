7082fe5b9e47b23dd23321d82342ced8e2bbca8c
kmorr102 <kendranmorris@gmail.com>
2024-01-29 12:09:27 -0600

    links and video content

--- src/client/components/work.jsx
+++ src/client/components/work.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { Routes,Route,Link,useNavigate } from "react-router-dom";
 import girlDinner from '/src/client/assets/girlDinner.png'
 import strangersThings from '/src/client/assets/strangersThings.png'
+import GirlDinnerDemo from '/src/client/assets/GirlDinner-FINAL.mp4'
+
 function Work(){
     
+    const navigateToGirlDinner=()=>{
+        window.location.href = 'https://github.com/kmorr102/Girl-Dinner'
+    }
+    const navigateToStrangersThingsGithub=()=>{
+        window.location.href= 'https://github.com/kmorr102/strangers-things'
+    }
+    const navigateToGirlDinnerVideo=() =>{
+        src={GirlDinnerDemo}
+    }
+    const navigateToStrangersThingsDemo=() =>{
+        window.location.href='https://sage-stroopwafel-c13960.netlify.app/'
+    }
 return(
     <div className='projects'>
         {/* <div className='header'> Kendra Morris </div> */}
         <img className='girlDinner' src={girlDinner} alt='girl dinner tech project' style={{width:'365px', height: '260px', border: 'black solid 3px'}}/>
-        <button>Demo</button> <button>Github</button>
+        <Link to="/Video" >
+        <button>Demo</button> </Link> <button onClick={navigateToGirlDinner}>Github</button>
+       
         <img className='strangersThings' src={strangersThings} alt='strangers things tech project' style={{width:'365px', height: '260px', border: 'black solid 3px'}}/>
-        <button>Demo</button> <button>Github</button>
+       
+        <button onClick={navigateToStrangersThingsDemo}>Demo</button> <button onClick={navigateToStrangersThingsGithub}>Github</button>
     </div>
 
 )
